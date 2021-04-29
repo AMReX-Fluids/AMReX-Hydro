13a7f185ed9557051ff6d00f7dc8aa7fe54b1f16
Michele Rosso <mrosso@lbl.gov>
2021-04-29 10:48:41 -0700

    MOL: provide RZ support as for Godunov

--- MOL/hydro_mol.cpp
+++ MOL/hydro_mol.cpp
@@ -42,10 +42,35 @@ MOL::ComputeAofs ( MultiFab& aofs, int aofs_comp, int ncomp,
                   AMREX_ALWAYS_ASSERT(yfluxes.nGrow() == yedge.nGrow());,
                   AMREX_ALWAYS_ASSERT(zfluxes.nGrow() == zedge.nGrow()););
 
+
     // To compute edge states, need at least 2 more ghost cells in state than in
     //  xedge
     if ( !known_edgestate )
         AMREX_ALWAYS_ASSERT(state.nGrow() >= xedge.nGrow()+2);
+
+#if (AMREX_SPACEDIM==2)
+    MultiFab* volume;
+    MultiFab* area[AMREX_SPACEDIM];
+
+    if ( geom.IsRZ() )
+    {
+        const DistributionMapping& dmap = aofs.DistributionMap();
+        const BoxArray& grids = aofs.boxArray();
+        const int ngrow_vol = aofs.nGrow();
+
+        volume = new MultiFab(grids,dmap,1,ngrow_vol);
+        geom.GetVolume(*volume);
+
+        const int ngrow_area = xfluxes.nGrow();
+
+        for (int dir = 0; dir < AMREX_SPACEDIM; ++dir)
+        {
+            BoxArray edge_ba(grids);
+            area[dir] = new MultiFab(edge_ba.surroundingNodes(dir),dmap,1,ngrow_area);
+            geom.GetFaceArea(*area[dir],dir);
+        }
+    }
+#endif
 
     Box  const& domain = geom.Domain();
 
@@ -84,16 +109,50 @@ MOL::ComputeAofs ( MultiFab& aofs, int aofs_comp, int ncomp,
 
         }
 
-        // Compute fluxes
-        HydroUtils::ComputeFluxes(gbx, AMREX_D_DECL(fx,fy,fz),
-                                  AMREX_D_DECL(u,v,w), AMREX_D_DECL(xed,yed,zed),
-                                  geom, ncomp );
+#if (AMREX_SPACEDIM == 2)
+	if ( geom.IsRZ() )
+	{
+            const auto& areax = area[0]->array(mfi);
+            const auto& areay = area[1]->array(mfi);
+            const auto& vol   = volume->array(mfi);
 
-        // Compute divergence
-        HydroUtils::ComputeDivergence(bx, aofs.array(mfi, aofs_comp), AMREX_D_DECL(fx,fy,fz),
-                                      AMREX_D_DECL( xed, yed, zed ), AMREX_D_DECL( u, v, w ),
-                                      ncomp, geom, iconserv.data());
+            HydroUtils::ComputeFluxesRZ( gbx,
+                                         AMREX_D_DECL( fx, fy, fz ),
+                                         AMREX_D_DECL( u, v, w ),
+                                         AMREX_D_DECL( xed, yed, zed ),
+                                         areax, areay,
+                                         ncomp );
 
+
+            HydroUtils::ComputeDivergenceRZ( bx,
+                                             aofs.array(mfi,aofs_comp),
+                                             AMREX_D_DECL( fx, fy, fz ),
+                                             AMREX_D_DECL( xed, yed, zed ),
+                                             AMREX_D_DECL( u, v, w ),
+                                             areax, areay, vol,
+                                             ncomp, iconserv.data() );
+
+	}
+	else
+#endif
+        {
+            // Compute fluxes
+            HydroUtils::ComputeFluxes( gbx,
+                                       AMREX_D_DECL(fx,fy,fz),
+                                       AMREX_D_DECL(u,v,w),
+                                       AMREX_D_DECL(xed,yed,zed),
+                                       geom, ncomp );
+
+            // Compute divergence
+            HydroUtils::ComputeDivergence( bx,
+                                           aofs.array(mfi, aofs_comp),
+                                           AMREX_D_DECL(fx,fy,fz),
+                                           AMREX_D_DECL( xed, yed, zed ),
+                                           AMREX_D_DECL( u, v, w ),
+                                           ncomp, geom, iconserv.data());
+        }
+
+        Gpu::streamSynchronize();  // otherwise we might be using too much memory
     }
 }
 
@@ -141,8 +200,32 @@ MOL::ComputeSyncAofs ( MultiFab& aofs, int aofs_comp, int ncomp,
     if ( !known_edgestate )
         AMREX_ALWAYS_ASSERT(state.nGrow() >= xedge.nGrow()+2);
 
+#if (AMREX_SPACEDIM==2)
+    MultiFab* volume;
+    MultiFab* area[AMREX_SPACEDIM];
+
+    if ( geom.IsRZ() )
+    {
+        const DistributionMapping& dmap = aofs.DistributionMap();
+        const BoxArray& grids = aofs.boxArray();
+        const int ngrow_vol = aofs.nGrow();
+
+        volume = new MultiFab(grids,dmap,1,ngrow_vol);
+        geom.GetVolume(*volume);
+
+        const int ngrow_area = xfluxes.nGrow();
+
+        for (int dir = 0; dir < AMREX_SPACEDIM; ++dir)
+        {
+            BoxArray edge_ba(grids);
+            area[dir] = new MultiFab(edge_ba.surroundingNodes(dir),dmap,1,ngrow_area);
+            geom.GetFaceArea(*area[dir],dir);
+        }
+    }
+#endif
+
     // Sync divergence computation is always conservative
-    std::vector<int>  iconserv(ncomp,1);
+    std::vector<int>  div_iconserv(ncomp,1);
 
     Box  const& domain = geom.Domain();
 
@@ -183,30 +266,60 @@ MOL::ComputeSyncAofs ( MultiFab& aofs, int aofs_comp, int ncomp,
 
         }
 
-        // Compute fluxes
-        HydroUtils::ComputeFluxes(bx, AMREX_D_DECL(fx,fy,fz),
-                                  AMREX_D_DECL(uc,vc,wc),
-                                  AMREX_D_DECL(xed,yed,zed), geom, ncomp );
-
-        // Compute divergence
+        // Temporary divergence
         Box tmpbox = amrex::surroundingNodes(bx);
         int tmpcomp = ncomp*AMREX_SPACEDIM;
         FArrayBox tmpfab(tmpbox, tmpcomp);
         Elixir eli = tmpfab.elixir();
         Array4<Real> divtmp_arr = tmpfab.array();
-        HydroUtils::ComputeDivergence(bx, divtmp_arr,
-                                      AMREX_D_DECL(fx,fy,fz),
-                                      AMREX_D_DECL( xed, yed, zed ),
-                                      AMREX_D_DECL( uc, vc, wc ),
-                                      ncomp, geom, iconserv.data() );
+
+#if (AMREX_SPACEDIM == 2)
+	if ( geom.IsRZ() )
+	{
+            const auto& areax = area[0]->array(mfi);
+            const auto& areay = area[1]->array(mfi);
+            const auto& vol   = volume->array(mfi);
+
+            HydroUtils::ComputeFluxesRZ( bx,
+                                         AMREX_D_DECL( fx, fy, fz ),
+                                         AMREX_D_DECL( uc, vc, wc ),
+                                         AMREX_D_DECL( xed, yed, zed ),
+                                         areax, areay,
+                                         ncomp );
+
+            HydroUtils::ComputeDivergenceRZ( bx, divtmp_arr,
+                                             AMREX_D_DECL( fx, fy, fz ),
+                                             AMREX_D_DECL( xed, yed, zed ),
+                                             AMREX_D_DECL( uc, vc, wc ),
+                                             areax, areay, vol,
+                                             ncomp, div_iconserv.data() );
+
+	}
+	else
+#endif
+        {
+            // Compute fluxes
+            HydroUtils::ComputeFluxes( bx,
+                                       AMREX_D_DECL(fx,fy,fz),
+                                       AMREX_D_DECL(uc,vc,wc),
+                                       AMREX_D_DECL(xed,yed,zed),
+                                       geom, ncomp );
+
+            // Compute divergence
+            HydroUtils::ComputeDivergence( bx, divtmp_arr,
+                                           AMREX_D_DECL(fx,fy,fz),
+                                           AMREX_D_DECL( xed, yed, zed ),
+                                           AMREX_D_DECL( uc, vc, wc ),
+                                           ncomp, geom, div_iconserv.data() );
+        }
 
         // Sum contribution to sync aofs
         auto const& aofs_arr = aofs.array(mfi, aofs_comp);
 
         amrex::ParallelFor(bx, ncomp, [aofs_arr, divtmp_arr]
         AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
-        {
-            aofs_arr( i, j, k, n ) += divtmp_arr( i, j, k, n );
-        });
+        { aofs_arr( i, j, k, n ) += divtmp_arr( i, j, k, n ); });
+
+        Gpu::streamSynchronize();  // otherwise we might be using too much memory
     }
 }
