a5a270e8f62c021144c0966e90e013f93199d603
Ann Almgren <asalmgren@lbl.gov>
2021-06-30 11:03:17 -0700

    Add doxygen headers

--- MOL/hydro_mol_extrap_vel_to_faces.cpp
+++ MOL/hydro_mol_extrap_vel_to_faces.cpp
@@ -2,11 +2,29 @@
 
 using namespace amrex;
 
-//
-// Compute upwinded FC velocities by extrapolating CC values in SPACE ONLY
-// This is NOT a Godunov type extrapolation: there is NO dependence on time!
-// The resulting FC velocities are computed at the CENTROID of the face.
-//
+/**
+ * \fn void MOL::ExtrapVelToFaces ( const MultiFab&  a_vel,
+                                    AMREX_D_DECL( MultiFab& a_umac,
+                                                  MultiFab& a_vmac,
+                                                  MultiFab& a_wmac ),
+                                    const Geometry&  a_geom,
+            			const Vector<BCRec>& h_bcrec,
+                                    BCRec  const* d_bcrec)
+ * \brief Compute upwinded FC velocities by extrapolating CC values in SPACE ONLY
+ *
+ * \param a_vel   The cell-centered velocity field
+ * \param a_umac  x-component of velocity on centroid of x-face
+ * \param a_vmac  y-component of velocity on centroid of y-face
+ * \param a_wmac  z-component of velocity on centroid of z-face
+ * \param a_geom  Geometry object at this level 
+ * \param h_bcrec Host version of BCRec
+ * \param d_bcrec Device version of BCRec
+ *
+ * Compute upwinded FC velocities by extrapolating CC values in SPACE ONLY
+ * This is NOT a Godunov type extrapolation: there is NO dependence on time!
+ * The resulting FC velocities are computed at the CENTROID of the face.
+ *
+ */
 void
 MOL::ExtrapVelToFaces ( const MultiFab&  a_vel,
                         AMREX_D_DECL( MultiFab& a_umac,
