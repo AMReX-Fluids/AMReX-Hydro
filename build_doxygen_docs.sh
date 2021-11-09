86e661ab8e0d20b7acd2a352ce45f3bf2449903d
etpalmer63 <etpalmer@math.sc.edu>
2021-11-09 10:46:16 -0500

    Link AMReX Doxygen docs via tagfile

--- build_doxygen_docs.sh
+++ build_doxygen_docs.sh
@@ -2,7 +2,12 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Doxygen
-echo "Build the Doxygen documentation"
+echo "Building the Doxygen documentation"
+
+# Get AMReX Doxygen Tagfile
+wget https://amrex-codes.github.io/amrex/docs_xml/doxygen/amrex-doxygen-web.tag.xml -O amrex-doxygen-web.tag.xml
+
+# Render HTML Docs
 doxygen ./Docs/Doxygen/doxygen.conf &> doxygen.out
 
 
