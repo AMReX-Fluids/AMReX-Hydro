2a3a58a3d85696fbcc684a738ffa51686d7db144
Erik Palmer <epalmer@lbl.gov>
2022-05-16 16:13:37 -0400

    Missing backslash typo in CUDA CI.

--- .github/workflows/dependencies/dependencies_CUDA10_2.sh
+++ .github/workflows/dependencies/dependencies_CUDA10_2.sh
@@ -18,7 +18,7 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" \
     | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
-sudo apt-get install -y
+sudo apt-get install -y          \
     cuda-command-line-tools-10-2 \
     cuda-compiler-10-2           \
     cuda-cupti-dev-10-2          \
