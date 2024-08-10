f82a4b503337e5f0e577220fa10d36d0ffc233a0
Ann Almgren <asalmgren@lbl.gov>
2024-08-10 16:29:17 -0700

    fix oops

--- EBGodunov/hydro_ebgodunov_extrap_vel_to_faces_3D.cpp
+++ EBGodunov/hydro_ebgodunov_extrap_vel_to_faces_3D.cpp
@@ -72,13 +72,16 @@ EBGodunov::ExtrapVelToFacesOnBox ( Box const& bx, int ncomp,
     amrex::ParallelFor(
         xebx, ncomp, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
         {
-            xlo(i,j,k,n) = Ipx(i-1,j,k,n);
-            xhi(i,j,k,n) = Imx(i  ,j,k,n);
+            Real lo = Ipx(i-1,j,k,n);
+            Real hi = Imx(i  ,j,k,n);
 
             const auto bc = HydroBC::getBC(i, j, k, n, domain, pbc, bc_arr);
 
-            EBGodunovBC::SetXBCs(i, j, k, n, q, xlo(i,j,k,n), xhi(i,j,k,n), u_ad(i,j,k), u_ad(i,j,k),
+            EBGodunovBC::SetXBCs(i, j, k, n, q, lo, hi, u_ad(i,j,k), u_ad(i,j,k),
                                  bc.lo(0), bc.hi(0), dlo.x, dhi.x, true);
+
+            xlo(i,j,k,n) = lo;
+            xhi(i,j,k,n) = hi;
 
             Real uad = u_ad(i,j,k);
             Real st = (uad >= 0.) ? lo : hi;
@@ -87,13 +90,16 @@ EBGodunov::ExtrapVelToFacesOnBox ( Box const& bx, int ncomp,
         },
         yebx, ncomp, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
         {
-            ylo(i,j,k,n) = Ipy(i,j-1,k,n);
-            yhi(i,j,k,n) = Imy(i,j  ,k,n);
+            Real lo = Ipy(i,j-1,k,n);
+            Real hi = Imy(i,j  ,k,n);
 
             const auto bc = HydroBC::getBC(i, j, k, n, domain, pbc, bc_arr);
 
-            EBGodunovBC::SetYBCs(i, j, k, n, q, ylo(i,j,k,n), yhi(i,j,k,n), v_ad(i,j,k), v_ad(i,j,k),
+            EBGodunovBC::SetYBCs(i, j, k, n, q, lo, hi, v_ad(i,j,k), v_ad(i,j,k),
                                  bc.lo(1), bc.hi(1), dlo.y, dhi.y, true);
+
+            ylo(i,j,k,n) = lo;
+            yhi(i,j,k,n) = hi;
 
             Real vad = v_ad(i,j,k);
             Real st = (vad >= 0.) ? lo : hi;
@@ -102,13 +108,16 @@ EBGodunov::ExtrapVelToFacesOnBox ( Box const& bx, int ncomp,
         },
         zebx, ncomp, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
         {
-            zlo = Ipz(i,j,k-1,n);
-            zhi = Imz(i,j,k  ,n);
+            Real lo = Ipz(i,j,k-1,n);
+            Real hi = Imz(i,j,k  ,n);
 
             const auto bc = HydroBC::getBC(i, j, k, n, domain, pbc, bc_arr);
 
-            EBGodunovBC::SetZBCs(i, j, k, n, q, zlo(i,j,k,n), zhi(i,j,k,n), w_ad(i,j,k), w_ad(i,j,k),
+            EBGodunovBC::SetZBCs(i, j, k, n, q, lo, hi, w_ad(i,j,k), w_ad(i,j,k),
                                  bc.lo(2), bc.hi(2), dlo.z, dhi.z, true);
+
+            zlo(i,j,k,n) = lo;
+            zhi(i,j,k,n) = hi;
 
             Real wad = w_ad(i,j,k);
             Real st = (wad >= 0.) ? lo : hi;
